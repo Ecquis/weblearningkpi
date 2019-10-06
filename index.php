5d17a066bc900596c5539718d8af1854e3970848
Unknown <omax1972@gmail.com>
2019-10-06 14:39:20 +0300

    Lab1 and TODO list

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Priceless</title>
+</head>
+<body>
+
+    <ul>
+        <li><a href="">Main</a></li>
+        <li><a href="">About us</a></li>
+        <li><a href="">Shop</a></li>
+        <li><a href="">Delivery</a></li>
+        <li><a href="">Contacts</a></li>
+    </ul>
+
+
+
+    <?php
+    phpinfo();
+    ?>
+
+</body>
+</html>
+
